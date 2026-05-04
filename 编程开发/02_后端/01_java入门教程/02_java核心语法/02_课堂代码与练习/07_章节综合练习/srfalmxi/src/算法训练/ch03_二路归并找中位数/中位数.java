971b0c201221dc44c1ec7ebc686d5e52aeac42a8
ShiJianDanYa <1446300258@qq.com>
2026-05-04 09:59:04 +0800

    vault backup: 2026-05-04 09:59:03

--- 编程开发/02_后端/01_java入门教程/02_java核心语法/02_课堂代码与练习/07_章节综合练习/srfalmxi/src/算法训练/ch03_二路归并找中位数/中位数.java
+++ 编程开发/02_后端/01_java入门教程/02_java核心语法/02_课堂代码与练习/07_章节综合练习/srfalmxi/src/算法训练/ch03_二路归并找中位数/中位数.java
@@ -16,12 +16,22 @@ public class 中位数 {
 		给定两个正序数组arr1和arr2，请先合并数组，并找出合并之后数组的中位数。举例:
 		1 2 3 4 5 6 7 8 9
 		中位数：5
-		12 34 56
+		1 2 3 4 5 6
 		中位数：（3+4）/ 2
 		*/
 		//定义两个数组
-		int[] arr1 = new int[] {1,2,3,4,5,6,7,8,9};
-		int[] arr2 = {}
+		int[] arr1 = new int[] {1,3,5,7,9};
+		int[] arr2 = {2,4,6};
+		//合并方法
+		merge(arr1,arr2);
+
+
+	}
+	public static int[] merge(int[] arr1,int[] arr2){
+		//创建新数组,用来存储两个数组的数据
+		int[] arr3 = new int[arr1.length+arr2.length];
+		//循环遍历数组
+
 
 	}
 }
